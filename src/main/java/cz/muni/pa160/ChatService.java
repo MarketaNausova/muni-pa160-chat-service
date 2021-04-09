fd29b93c84a2b6bd3b6e1e689413e1e01784c010
MarketaNausova <48683700+MarketaNausova@users.noreply.github.com>
2021-04-09 10:13:03 +0200

    Update ChatService.java

--- src/main/java/cz/muni/pa160/ChatService.java
+++ src/main/java/cz/muni/pa160/ChatService.java
@@ -1,6 +1,5 @@
 package cz.muni.pa160;
 
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
